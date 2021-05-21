42754c2ff4a6004e6aebfdd01796e2cfd539bc96
jimalz <67662377+jimalz@users.noreply.github.com>
2021-05-20 23:53:55 -0500

    another one

---
+++ xmlrpc.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<enginename>WordPress</enginename>
+		<enginelink>https://wordpress.org/</enginelink>
+		<homepagelink>https://connecteam.com</homepagelink>
+		<apis>
+			<api name="WordPress" blogid="1" preferred="true" apilink="https://connecteam.com/xmlrpc.php" />
+			<api name="Movable Type" blogid="1" preferred="false" apilink="https://connecteam.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogid="1" preferred="false" apilink="https://connecteam.com/xmlrpc.php" />
+			<api name="Blogger" blogid="1" preferred="false" apilink="https://connecteam.com/xmlrpc.php" />
+				<api name="WP-API" blogid="1" preferred="false" apilink="https://connecteam.com/wp-json/" />
+			</apis>
+	</service>
+</rsd>
